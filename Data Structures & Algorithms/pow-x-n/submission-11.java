4ed20cf08201e06ca24b0fddc97f47ece1f732b1
Aditya Bisht <adityabishtofficial547@gmail.com>
2026-08-29 02:46:19 +0530

    Add: pow-x-n - submission-11

---
+++ Data Structures & Algorithms/pow-x-n/submission-11.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;
+        double res = 1;
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        while (power > 0) {
+            if (power % 2 == 1) {
+                res *= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+        return res;
+    }
+}
